63a269324ffe998655b756960cb691f0415d18b6
Lucas Daniel Beltrame <lucasdanielbeltrame@hotmail.com>
2025-02-12 21:13:56 -0300

    gerenciamento do portifolio em pt eng

--- resources/views/dashboard/viewCreateUpdateWorkPage.blade.php
+++ resources/views/dashboard/viewCreateUpdateWorkPage.blade.php
@@ -40,16 +40,20 @@
                             <!-- form start -->
                             <form id="quickForm">
                                 <div class="card-body first-card-body">
-                                    <div class="form-group">
-                                        <label for="InputWorkTitle">Title</label>
-                                        <input type="text" name="workTitle" class="form-control" id="InputWorkTitle" value="{{ isset($work->title) ? $work->title : '' }}">
-                                    </div>
+                                    @include('components.inputtextpteng',[
+                                    'title' => 'Title',
+                                    'id_input_text' => 'InputWorkTitle',
+                                    'arg_value' => $work->title ? $work->title : '',
+                                    'arg_value_eng' => $work->title_eng ? $work->title_eng : ''
+                                    ])
                                 </div>
-                                <div class="card-body first-card-body">
-                                    <div class="form-group">
-                                        <label for="InputWorkDescription">Description</label>
-                                        <input type="text" name="workDescription" class="form-control" id="InputWorkDescription" value="{{ isset($work->description) ? $work->description : '' }}">
-                                    </div>
+                                <div class="card-body">
+                                    @include('components.inputtextpteng',[
+                                    'title' => 'Description',
+                                    'id_input_text' => 'InputWorkDescription',
+                                    'arg_value' => $work->description ? $work->description : '',
+                                    'arg_value_eng' => $work->description_eng ? $work->description_eng : ''
+                                    ])
                                 </div>
                                 <!-- 11111 -->
                                 <div class="card-body">
@@ -64,17 +68,21 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="card-body first-card-body">
-                                    <div class="form-group">
-                                        <label for="InputWorkType1">Type 1</label>
-                                        <input type="text" name="workType1" class="form-control" id="InputWorkType1" value="{{ isset($work->type_1) ? $work->type_1 : '' }}">
-                                    </div>
+                                <div class="card-body">
+                                    @include('components.inputtextpteng',[
+                                    'title' => 'Type 1',
+                                    'id_input_text' => 'InputWorkType1',
+                                    'arg_value' => $work->type_1 ? $work->type_1 : '',
+                                    'arg_value_eng' => $work->type_1_eng ? $work->type_1_eng : ''
+                                    ])
                                 </div>
-                                <div class="card-body first-card-body">
-                                    <div class="form-group">
-                                        <label for="InputWorkTitle1">Title 1</label>
-                                        <input type="text" name="workTitle1" class="form-control" id="InputWorkTitle1" value="{{ isset($work->title_1) ? $work->title_1 : '' }}">
-                                    </div>
+                                <div class="card-body">
+                                    @include('components.inputtextpteng',[
+                                    'title' => 'Title 1',
+                                    'id_input_text' => 'InputWorkTitle1',
+                                    'arg_value' => $work->title_1 ? $work->title_1 : '',
+                                    'arg_value_eng' => $work->title_1_eng ? $work->title_1_eng : ''
+                                    ])
                                 </div>
                                 <!-- 22222 -->
                                 <div class="card-body">
@@ -89,17 +97,21 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="card-body first-card-body">
-                                    <div class="form-group">
-                                        <label for="InputWorkType2">Type 2</label>
-                                        <input type="text" name="workType2" class="form-control" id="InputWorkType2" value="{{ isset($work->type_2) ? $work->type_2 : '' }}">
-                                    </div>
+                                <div class="card-body">
+                                    @include('components.inputtextpteng',[
+                                    'title' => 'Type 2',
+                                    'id_input_text' => 'InputWorkType2',
+                                    'arg_value' => $work->type_2 ? $work->type_2 : '',
+                                    'arg_value_eng' => $work->type_2_eng ? $work->type_2_eng : ''
+                                    ])
                                 </div>
-                                <div class="card-body first-card-body">
-                                    <div class="form-group">
-                                        <label for="InputWorkTitle2">Title 2</label>
-                                        <input type="text" name="workTitle2" class="form-control" id="InputWorkTitle2" value="{{ isset($work->title_2) ? $work->title_2 : '' }}">
-                                    </div>
+                                <div class="card-body">
+                                    @include('components.inputtextpteng',[
+                                    'title' => 'Title 2',
+                                    'id_input_text' => 'InputWorkTitle2',
+                                    'arg_value' => $work->title_2 ? $work->title_2 : '',
+                                    'arg_value_eng' => $work->title_2_eng ? $work->title_2_eng : ''
+                                    ])
                                 </div>
                                 <!-- 33333 -->
                                 <div class="card-body">
@@ -114,17 +126,21 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="card-body first-card-body">
-                                    <div class="form-group">
-                                        <label for="InputWorkType3">Type 3</label>
-                                        <input type="text" name="workType3" class="form-control" id="InputWorkType3" value="{{ isset($work->type_3) ? $work->type_3 : '' }}">
-                                    </div>
+                                <div class="card-body">
+                                    @include('components.inputtextpteng',[
+                                    'title' => 'Type 3',
+                                    'id_input_text' => 'InputWorkType3',
+                                    'arg_value' => $work->type_3 ? $work->type_3 : '',
+                                    'arg_value_eng' => $work->type_3_eng ? $work->type_3_eng : ''
+                                    ])
                                 </div>
-                                <div class="card-body first-card-body">
-                                    <div class="form-group">
-                                        <label for="InputWorkTitle3">Title 3</label>
-                                        <input type="text" name="workTitle3" class="form-control" id="InputWorkTitle3" value="{{ isset($work->title_3) ? $work->title_3 : '' }}">
-                                    </div>
+                                <div class="card-body">
+                                    @include('components.inputtextpteng',[
+                                    'title' => 'Title 3',
+                                    'id_input_text' => 'InputWorkTitle3',
+                                    'arg_value' => $work->title_3 ? $work->title_3 : '',
+                                    'arg_value_eng' => $work->title_3_eng ? $work->title_3_eng : ''
+                                    ])
                                 </div>
                                 <!-- 44444 -->
                                 <div class="card-body">
@@ -139,17 +155,21 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="card-body first-card-body">
-                                    <div class="form-group">
-                                        <label for="InputWorkType4">Type 4</label>
-                                        <input type="text" name="workType4" class="form-control" id="InputWorkType4" value="{{ isset($work->type_4) ? $work->type_4 : '' }}">
-                                    </div>
+                                <div class="card-body">
+                                    @include('components.inputtextpteng',[
+                                    'title' => 'Type 4',
+                                    'id_input_text' => 'InputWorkType4',
+                                    'arg_value' => $work->type_4 ? $work->type_4 : '',
+                                    'arg_value_eng' => $work->type_4_eng ? $work->type_4_eng : ''
+                                    ])
                                 </div>
-                                <div class="card-body first-card-body">
-                                    <div class="form-group">
-                                        <label for="InputWorkTitle4">Title 4</label>
-                                        <input type="text" name="workTitle4" class="form-control" id="InputWorkTitle4" value="{{ isset($work->title_4) ? $work->title_4 : '' }}">
-                                    </div>
+                                <div class="card-body">
+                                    @include('components.inputtextpteng',[
+                                    'title' => 'Title 4',
+                                    'id_input_text' => 'InputWorkTitle4',
+                                    'arg_value' => $work->title_4 ? $work->title_4 : '',
+                                    'arg_value_eng' => $work->title_4_eng ? $work->title_4_eng : ''
+                                    ])
                                 </div>
                                 <!-- 55555 -->
                                 <div class="card-body">
@@ -164,17 +184,21 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="card-body first-card-body">
-                                    <div class="form-group">
-                                        <label for="InputWorkType5">Type 5</label>
-                                        <input type="text" name="workType5" class="form-control" id="InputWorkType5" value="{{ isset($work->type_5) ? $work->type_5 : '' }}">
-                                    </div>
+                                <div class="card-body">
+                                    @include('components.inputtextpteng',[
+                                    'title' => 'Type 5',
+                                    'id_input_text' => 'InputWorkType5',
+                                    'arg_value' => $work->type_5 ? $work->type_5 : '',
+                                    'arg_value_eng' => $work->type_5_eng ? $work->type_5_eng : ''
+                                    ])
                                 </div>
-                                <div class="card-body first-card-body">
-                                    <div class="form-group">
-                                        <label for="InputWorkTitle5">Title 5</label>
-                                        <input type="text" name="workTitle5" class="form-control" id="InputWorkTitle5" value="{{ isset($work->title_5) ? $work->title_5 : '' }}">
-                                    </div>
+                                <div class="card-body">
+                                    @include('components.inputtextpteng',[
+                                    'title' => 'Title 5',
+                                    'id_input_text' => 'InputWorkTitle5',
+                                    'arg_value' => $work->title_5 ? $work->title_5 : '',
+                                    'arg_value_eng' => $work->title_5_eng ? $work->title_5_eng : ''
+                                    ])
                                 </div>
                                 <!-- 66666 -->
                                 <div class="card-body">
@@ -189,17 +213,21 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="card-body first-card-body">
-                                    <div class="form-group">
-                                        <label for="InputWorkType6">Type 6</label>
-                                        <input type="text" name="workType6" class="form-control" id="InputWorkType6" value="{{ isset($work->type_6) ? $work->type_6 : '' }}">
-                                    </div>
+                                <div class="card-body">
+                                    @include('components.inputtextpteng',[
+                                    'title' => 'Type 6',
+                                    'id_input_text' => 'InputWorkType6',
+                                    'arg_value' => $work->type_6 ? $work->type_6 : '',
+                                    'arg_value_eng' => $work->type_6_eng ? $work->type_6_eng : ''
+                                    ])
                                 </div>
-                                <div class="card-body first-card-body">
-                                    <div class="form-group">
-                                        <label for="InputWorkTitle6">Title 6</label>
-                                        <input type="text" name="workTitle6" class="form-control" id="InputWorkTitle6" value="{{ isset($work->title_6) ? $work->title_6 : '' }}">
-                                    </div>
+                                <div class="card-body">
+                                    @include('components.inputtextpteng',[
+                                    'title' => 'Title 6',
+                                    'id_input_text' => 'InputWorkTitle6',
+                                    'arg_value' => $work->title_6 ? $work->title_6 : '',
+                                    'arg_value_eng' => $work->title_6_eng ? $work->title_6_eng : ''
+                                    ])
                                 </div>
                                 <!-- 77777 -->
                                 <div class="card-body">
@@ -214,17 +242,21 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="card-body first-card-body">
-                                    <div class="form-group">
-                                        <label for="InputWorkType7">Type 7</label>
-                                        <input type="text" name="workType7" class="form-control" id="InputWorkType7" value="{{ isset($work->type_7) ? $work->type_7 : '' }}">
-                                    </div>
+                                <div class="card-body">
+                                    @include('components.inputtextpteng',[
+                                    'title' => 'Type 7',
+                                    'id_input_text' => 'InputWorkType7',
+                                    'arg_value' => $work->type_7 ? $work->type_7 : '',
+                                    'arg_value_eng' => $work->type_7_eng ? $work->type_7_eng : ''
+                                    ])
                                 </div>
-                                <div class="card-body first-card-body">
-                                    <div class="form-group">
-                                        <label for="InputWorkTitle7">Title 7</label>
-                                        <input type="text" name="workTitle7" class="form-control" id="InputWorkTitle7" value="{{ isset($work->title_7) ? $work->title_7 : '' }}">
-                                    </div>
+                                <div class="card-body">
+                                    @include('components.inputtextpteng',[
+                                    'title' => 'Title 7',
+                                    'id_input_text' => 'InputWorkTitle7',
+                                    'arg_value' => $work->title_7 ? $work->title_7 : '',
+                                    'arg_value_eng' => $work->title_7_eng ? $work->title_7_eng : ''
+                                    ])
                                 </div>
 
                                 <div class="card-footer">
