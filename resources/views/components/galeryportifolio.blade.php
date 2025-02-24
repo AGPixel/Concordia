5d2663634d1dc2002c0c95641dffa6df0fa50de6
Lucas Daniel Beltrame <lucasdanielbeltrame@hotmail.com>
2025-02-24 08:05:54 -0300

    fix portifolio

--- resources/views/components/galeryportifolio.blade.php
+++ resources/views/components/galeryportifolio.blade.php
@@ -1,8 +1,10 @@
 @if(isset($img))
 <a class="link-pop fj-gallery-item" href="{{asset("../storage/$img")}}" data-fancybox="_1" data-caption="{{ $title }}">
     <img class="" src="{{asset("../storage/$img")}}" alt="">
+    @if(isset($type))
     <div class="cap">
         <span>{{ $type }}</span>
     </div>
+    @endif
 </a>
 @endif
