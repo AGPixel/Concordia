4ec45d75f9c2f4c1213c59ad67a0c2e1d477617c
Lucas Daniel Beltrame <lucasdanielbeltrame@hotmail.com>
2025-02-26 08:16:48 -0300

    Correção ao criar portifolio

--- resources/views/components/metatags.blade.php
+++ resources/views/components/metatags.blade.php
@@ -2,17 +2,17 @@
     @include('components.inputtext',['flag' => 'pteng',
     'title' => 'Meta tags Title',
     'id_input_text' => 'InputMetaTagsTitle',
-    'arg_value' => $arg_object->meta_tags_title ? $arg_object->meta_tags_title : '' ])
+    'arg_value' => isset($arg_object) ? (isset($arg_object->meta_tags_title) ? $arg_object->meta_tags_title : '') : '' ])
 </div>
 <div class="card-body first-card-body">
     @include('components.inputtext',['flag' => 'pteng',
     'title' => 'Meta tags Description',
     'id_input_text' => 'InputMetaTagsDescription',
-    'arg_value' => $arg_object->meta_tags_description ? $arg_object->meta_tags_description : '' ])
+    'arg_value' => isset($arg_object) ? (isset($arg_object->meta_tags_description) ? $arg_object->meta_tags_description : '') : '' ])
 </div>
 <div class="card-body first-card-body">
     @include('components.inputtext',['flag' => 'pteng',
     'title' => 'Meta tags Keywords',
     'id_input_text' => 'InputMetaTagsKeywords',
-    'arg_value' => $arg_object->meta_tags_keywords ? $arg_object->meta_tags_keywords : '' ])
+    'arg_value' => isset($arg_object) ? (isset($arg_object->meta_tags_keywords) ? $arg_object->meta_tags_keywords : '') : '' ])
 </div>
